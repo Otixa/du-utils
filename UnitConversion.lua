548c4ca28b2a5e5bffafc2f907ba5a8f0ecc620c
Rainfall <rainfey0+git@gmail.com>
2020-09-12 00:17:01 +0100

    Add braking simulator
    Add Templating engine
    Add variable distance, acceleration and time

--- UnitConversion.lua
+++ UnitConversion.lua
@@ -18,6 +18,49 @@ function UnitConversion()
         return string.format("%s%s%s", val, "", suffix)
     end
 
+    function self.VariableDistance(input)
+        local self = {Value = input}
+        local mt = {
+            __tostring = function(t) 
+                local v = t.Value
+                local suffix = "m"
+                if v > 200000 then v = v/200000 suffix = "su"
+                elseif v > 1000 then v = v/1000 suffix = "km" end
+                return Utilities.Round(v,2)..suffix
+            end,
+            __add = function(t, v) if type(t)=="table" then return t.Value+v else return v.Value+t end end,
+            __sub = function(t, v)  if type(t)=="table" then return t.Value-v else return v.Value-t end end,
+            __mul = function(t, v)  if type(t)=="table" then return t.Value*v else return v.Value*t end end,
+            __div = function(t, v)  if type(t)=="table" then return t.Value/v else return v.Value/t end end,
+            __pow = function(t, v)  if type(t)=="table" then return t.Value^v else return v.Value^t end end,
+            __lt = function(t, v)  if type(t)=="table" then return t.Value<v else return v.Value<t end end,
+            __le = function(t, v)  if type(t)=="table" then return t.Value<=v else return v.Value<=t end end,
+        }
+        setmetatable(self, mt)
+        return self
+    end
+    function self.VariableVelocity(input)
+        local self = {Value = input}
+        local mt = {
+            __tostring = function(t) 
+                local v = t.Value
+                local suffix = "m/s"
+                if v > 200000 then v = v/200000 suffix = "su/s"
+                elseif v > 1000 then v = v/1000 suffix = "km/s" end
+                return Utilities.Round(v,2)..suffix
+            end,
+            __add = function(t, v) if type(t)=="table" then return t.Value+v else return v.Value+t end end,
+            __sub = function(t, v)  if type(t)=="table" then return t.Value-v else return v.Value-t end end,
+            __mul = function(t, v)  if type(t)=="table" then return t.Value*v else return v.Value*t end end,
+            __div = function(t, v)  if type(t)=="table" then return t.Value/v else return v.Value/t end end,
+            __pow = function(t, v)  if type(t)=="table" then return t.Value^v else return v.Value^t end end,
+            __lt = function(t, v)  if type(t)=="table" then return t.Value<v else return v.Value<t end end,
+            __le = function(t, v)  if type(t)=="table" then return t.Value<=v else return v.Value<=t end end,
+        }
+        setmetatable(self, mt)
+        return self
+    end
+
     function self.TimeConversion(seconds)
         local ret = {
             Months = 0,
@@ -26,23 +69,24 @@ function UnitConversion()
             Minutes = 0,
             Seconds = 0,
         }
-        ret.__index = ret
-        function ret:__tostring()
-            local ret = ""
-            if self.Months > 0 then ret = ret .. self.Months .. " Months " end
-            if self.Days > 0 then ret = ret .. self.Days .. " Days " end
-
-            local hours_pad = self.Hours
-            if #tostring(hours_pad)<2 then hours_pad = "0"..hours_pad end
-            local minutes_pad = self.Minutes
-            if #tostring(minutes_pad)<2 then minutes_pad = "0"..minutes_pad end
-            local seconds_pad = self.Seconds
-            if #tostring(seconds_pad)<2 then seconds_pad = "0"..seconds_pad end
-
-            ret = ret .. string.format(" %s:%s:%s", hours_pad, minutes_pad, seconds_pad)
-            return ret
-        end
-
+        local mt = {
+            __tostring = function(self)
+                local ret = ""
+                if self.Months > 0 then ret = ret .. self.Months .. " Months " end
+                if self.Days > 0 then ret = ret .. self.Days .. " Days " end
+    
+                local hours_pad = self.Hours
+                if #tostring(hours_pad)<2 then hours_pad = "0"..hours_pad end
+                local minutes_pad = self.Minutes
+                if #tostring(minutes_pad)<2 then minutes_pad = "0"..minutes_pad end
+                local seconds_pad = self.Seconds
+                if #tostring(seconds_pad)<2 then seconds_pad = "0"..seconds_pad end
+    
+                ret = ret .. string.format(" %s:%s:%s", hours_pad, minutes_pad, seconds_pad)
+                return ret
+            end
+        }
+        setmetatable(ret, mt)
         ret.Months = math.floor(seconds/2592000)
         seconds = seconds % 2592000
         ret.Days = math.floor(seconds/86400)
@@ -50,8 +94,7 @@ function UnitConversion()
         ret.Hours = math.floor(seconds / 3600)
         seconds = seconds % 3600
         ret.Minutes = math.floor(seconds/60)
-        ret.Seconds = seconds % 60
-
+        ret.Seconds = Utilities.Round(seconds % 60,0)
         return ret
     end
 
