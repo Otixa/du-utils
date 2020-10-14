d20890c49d3fac54029c937992a47f19888cfa35
Otixa <admin@shadowtemplar.org>
2020-10-14 10:16:31 +0000

    Added State Machine classes

---
+++ StateMachine.lua
@@ -0,0 +1,33 @@
+StateMachine = (function()
+    local this = {}
+    this.Current = nil
+    this.Update = function()
+        local state = this.Current
+        if state then
+            if state.Condition() then 
+                state.End()
+                if state.Next ~= nil then
+                    system.print("State change: " .. state.Next.Name)
+                    this.Current = state.Next
+                    state.Next.Start()
+                end
+            else
+                state.Action()
+            end
+        end
+    end
+    return this
+end)
+
+State = (function(stateMachine, name, condition, action, nextState)
+    local this = {}
+    setmetatable(this, {__call = function(ref, ...) ref.Update(...) end, _name = "State" })
+    this.Name = name or "State"
+    this.StateMachine = stateMachine
+    this.Next = nextState
+    this.Condition = condition or function() return true end
+    this.Start = function() end
+    this.End = function() end
+    this.Action = action or function() end
+    return this
+end)
