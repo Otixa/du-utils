a7cfd8af13014667b3d641f59858cd2d2644e155
Otixa <admin@shadowtemplar.org>
2020-11-26 16:40:39 +0000

    Update PlanetaryUtils.lua

--- PlanetaryUtils.lua
+++ PlanetaryUtils.lua
@@ -1,10 +1,8 @@
---@class PlanetaryUtils
 --@require Planets
 
+--@class PlanetaryUtils
 PlanetaryUtils = (function ()
 	local this = {}
-	local deg2rad    = math.pi/180
-	local rad2deg    = 180/math.pi
 	
 	function this.FromGamePos(pos)
 		local pattern = "::pos{(%d+),(%d+),([+-]?%d+%.?%d*),([+-]?%d+%.?%d*),([+-]?%d+%.?%d*)}"
@@ -18,12 +16,12 @@ PlanetaryUtils = (function ()
 			Altitude  = tonumber(altitude)
 		}
 		
-		local xproj = math.cos(posTable.Latitude * deg2rad)
+		local xproj = math.cos(posTable.Latitude * constants.deg2rad)
 		posTable.Cartesian = vec3(Planets[posTable.PlanetId].Position) + (
 			(Planets[posTable.PlanetId].WaterRadius + posTable.Altitude) * 
-			vec3(xproj * math.cos(posTable.Longitude * deg2rad),
-				xproj * math.sin(posTable.Longitude * deg2rad),
-				math.sin(posTable.Latitude * deg2rad))
+			vec3(xproj * math.cos(posTable.Longitude * constants.deg2rad),
+				xproj * math.sin(posTable.Longitude * constants.deg2rad),
+				math.sin(posTable.Latitude * constants.deg2rad))
 		)
 		
 		return setmetatable(posTable,{
@@ -46,8 +44,8 @@ PlanetaryUtils = (function ()
 		local altitude = distance - Planets[planetId].WaterRadius
 		
 		local phi = math.atan(posRelative.y,posRelative.x)
-		local longitude = ((phi <= 0) and phi or (2 * math.pi + phi)) * rad2deg
-		local latitude = ((math.pi / 2) - math.acos(posRelative.z / distance)) * rad2deg
+		local longitude = ((phi <= 0) and phi or (2 * math.pi + phi)) * constants.deg2rad
+		local latitude = ((math.pi / 2) - math.acos(posRelative.z / distance)) * constants.deg2rad
 		
 		return setmetatable({
 			SystemId  = systemId,
