a2d28d310c47f03bbf04f08fa59210ea64f53024
Otixa <admin@shadowtemplar.org>
2021-12-18 08:50:39 +0000

    Update StringExtensions.lua

--- StringExtensions.lua
+++ StringExtensions.lua
@@ -4,22 +4,52 @@ local stringmt = getmetatable("")
 
 stringmt.__index["parseCommand"] = function(str)
     local out = {}
-    local e = 0
+    local tail = 0
+    local rollback = 0
+    local special = {';','|','>>','>','<','&&','&','||'}
+    local specialConcat = table.concat(special, "")
     while true do
-        local b = e+1
-        b = str:find("%S",b)
-        if b==nil then break end
-        if str:sub(b,b)=="'" then
-            e = str:find("'",b+1)
-            b = b+1
-        elseif str:sub(b,b)=='"' then
-            e = str:find('"',b+1)
-            b = b+1
+        local start = tail+1-rollback
+        rollback = 0
+        start = str:find("%S",start)
+        if start==nil then break end
+        if str:sub(start,start)=="'" then
+            tail = str:find("'",start+1)
+            start = start+1
+        elseif str:sub(start,start)=='"' then
+            tail = str:find('"',start+1)
+            start = start+1
         else
-            e = str:find("%s",b+1)
+            local isSpecial = false
+            for i=1,#special do
+                local chr = special[i]
+                local first = str:sub(start,start+(#chr-1))
+                if chr == first then
+                    tail = start+#chr
+                    start = start
+                    rollback = 1
+                    isSpecial = true
+                    break
+                end
+            end
+            if not isSpecial then
+                ---check if contains special
+                local sTail = str:find("["..specialConcat.."]",start+1)
+                local nTail = str:find("%s",start+1)
+                if sTail then
+                    if sTail <= nTail then
+                        tail = sTail
+                        rollback = 1
+                    else
+                        tail = nTail
+                    end
+                else
+                    tail = nTail
+                end
+            end
         end
-        if e==nil then e=#str+1 end
-        local parsed = str:sub(b,e-1)
+        if tail==nil then tail=#str+1 end
+        local parsed = str:sub(start,tail-1)
         if parsed then
             out[#out+1] = parsed:gsub("\\\"", "\"")
         end
